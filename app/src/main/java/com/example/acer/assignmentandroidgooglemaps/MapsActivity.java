c75cefa3734486529b2a34857dd457765eef87c0
darwish791 <darwish791@gmail.com>
2018-12-21 02:08:04 +0800

    Initial commit replace mockPackager with getPackageManager

---
+++ app/src/main/java/com/example/acer/assignmentandroidgooglemaps/MapsActivity.java
@@ -0,0 +1,111 @@
+package com.example.acer.assignmentandroidgooglemaps;
+
+import android.Manifest;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+//import android.test.mock.MockPackageManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private Double latitude, longitude;
+
+    Button btnShowLocation;
+    private static final int REQUEST_CODE_PERMISSION = 2;
+    String mPermission = Manifest.permission.ACCESS_FINE_LOCATION;
+
+    // GPSTracker class
+    com.example.acer.assignmentandroidgooglemaps.GPSTracker gps;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+
+        try {
+            if (ActivityCompat.checkSelfPermission(this, mPermission)
+                    != getPackageManager().PERMISSION_GRANTED) {
+
+                ActivityCompat.requestPermissions(this, new String[]{mPermission},
+                        REQUEST_CODE_PERMISSION);
+
+                // If any permission above not allowed by user, this condition will
+                //  execute every time, else your else part will work
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        btnShowLocation = (Button) findViewById(R.id.button);
+
+        // show location button click event
+        btnShowLocation.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                // create class object
+                gps = new com.example.acer.assignmentandroidgooglemaps.GPSTracker(MapsActivity.this);
+
+                // check if GPS enabled
+                if(gps.canGetLocation()){
+
+                    latitude = gps.getLatitude();
+                    longitude = gps.getLongitude();
+
+                    Toast.makeText(getApplicationContext(), "Lokasi anda ialah - \nLatitud: "
+                            + latitude + "\nLongitud: " + longitude, Toast.LENGTH_LONG).show();
+                    ShowMap();
+                }else{
+                    // can't get location
+                    // GPS or Network is not enabled
+                    // Ask user to enable GPS/network in settings
+                    gps.showSettingsAlert();
+                }
+            }
+        });
+    }
+
+    public void ShowMap() {
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+
+        googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+
+        // Add a marker in current location and move the camera
+        LatLng mylocation = new LatLng(latitude, longitude);
+        mMap.addMarker(new MarkerOptions().position(mylocation).title("My Location"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(mylocation, 15));
+
+
+    }
+}
